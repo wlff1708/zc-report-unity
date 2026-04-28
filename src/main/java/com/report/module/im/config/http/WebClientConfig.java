9635b4c30dc882c23164c6920aa3e050ffde88f6
gw-ubuntu22 <gw-ubuntu22@123.com>
2026-04-28 21:41:25 +0800

    feat(im): 添加 WebClientConfig 配置类

---
+++ src/main/java/com/report/module/im/config/http/WebClientConfig.java
@@ -0,0 +1,23 @@
+package com.report.module.im.config.http;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+/**
+ * WebClient + @HttpExchange 配置
+ */
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+
+    @Bean
+    public HttpServiceProxyFactory httpServiceProxyFactory(WebClient webClient) {
+        return HttpServiceProxyFactory.builderFor(webClient).build();
+    }
+}
